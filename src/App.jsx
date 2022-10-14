173ebbe1015c16404083390a1cf1e3e3328eb78b
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-10-14 13:20:59 -0400

    removed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import IncomeForm from "./components/IncomeForm";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import ExpenseForm from "./components/expenseForm";
+import ExpenseForm from "./components/ExpenseForm";
 
 function App() {
   return (
