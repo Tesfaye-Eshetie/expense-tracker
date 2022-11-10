2fe3c8a83ed5fe8e9ccc600d26be74014313df4a
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-11-10 15:49:23 -0500

    updated FAQ component

--- src/components/DisplayFAQs.jsx
+++ src/components/DisplayFAQs.jsx
@@ -1,24 +1,13 @@
-import React from "react";
-import Accordion from "react-bootstrap/Accordion";
-import Card from "react-bootstrap/Card";
+import { Accordion } from "./Accordion";
 import FAQs from "../data/FAQs.json";
 
 export default function DisplayFAQs() {
   return (
-    <Card className="m-4">
-      <Card.Body className="p-4">
-        <Card.Title className="text-capitalize pb-3 fw-bolder">
-          Frequently asked questions
-        </Card.Title>
-        <Accordion defaultActiveKey="1">
-          {FAQs.map((FAQ) => (
-            <Accordion.Item eventKey={FAQ.id} key={FAQ.id}>
-              <Accordion.Header>{FAQ.Question}</Accordion.Header>
-              <Accordion.Body>{FAQ.Answer}</Accordion.Body>
-            </Accordion.Item>
-          ))}
-        </Accordion>
-      </Card.Body>
-    </Card>
+    <div className="accordion" id="faq">
+      <h3 className="accordion__title">Frequently asked questions</h3>
+      {FAQs.map((FAQ) => (
+        <Accordion key={FAQ.id} Question={FAQ.Question} Answer={FAQ.Answer} />
+      ))}
+    </div>
   );
 }
