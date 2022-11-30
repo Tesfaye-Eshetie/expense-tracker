a7ca4808599c61caf6557e6ba934d1e319b42ad5
Tesfaye-Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-11-30 04:11:25 +0000

    Deploying to gh-pages from @ Tesfaye-Eshetie/expense-tracker@755a94593e36b2afc88fbace6f24d2546f9432d1 🚀

--- serviceworker.js
+++ serviceworker.js
@@ -1,49 +1,60 @@
+var CACHE_STATIC_NAME = "static-v1";
+var CACHE_DYNAMIC_NAME = "dynamic-v1";
+
 // Install SW
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
-    caches.open("static-v1").then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll([
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log("[Service Worker] Precaching App Shell");
+      cache.addAll([
         "/",
         "index.html",
+        "src/App.css",
+        "src/App.jsx",
+        "src/main.jsx",
+        "src/assets/images/ExpenseIt.png",
+        "src/utilities/formatCurrency.js",
+        "src/data/FAQs.json",
       ]);
     })
   );
 });
 
+// Activate the SW
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log("[Service Worker] Removing old cache.", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
 // Listen for requests
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(function (response) {
       if (response) {
         return response;
       } else {
-        return fetch(event.request).then((res) => {
-          return caches.open("dynamic-v1").then((cache) => {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function (err) {});
       }
     })
-  );
-});
-
-// Activate the SW
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push("static-v1");
-  cacheWhitelist.push("dynamic-v1");
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
   );
 });
